a23a7ceea1ddd042f695d03834176c5d93e1883d
ArgueDev <christianarguelloo97@gmail.com>
2024-12-12 16:03:29 -0500

    Proyecto Agencia de Viaje

---
+++ controllers/testimonialControllers.js
@@ -0,0 +1,56 @@
+import { Testimonial } from '../models/Testimoniales.js';
+
+const guardarTestimonial = async (req, res) => {
+    // Validar
+    const { nombre, correo, mensaje } = req.body;
+
+    const errores = [];
+
+    if (nombre.trim() === '') {
+        errores.push({mensaje: 'El nombre no puede estar vacio'});
+    }
+
+    if (correo.trim() === '') {
+        errores.push({mensaje: 'El correo no puede estar vacio'});
+    }
+
+    if (mensaje.trim() === '') {
+        errores.push({mensaje: 'El mensaje no puede estar vacio'});
+    }
+
+    if (errores.length > 0) { 
+
+        //  Consultar si ya existe el testimonial
+        const testimoniales = await Testimonial.findAll();
+
+        // Mostrar la vista con errores
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje,
+            testimoniales
+        })
+    } else {
+        // Almacenar en la base de datos
+        try {
+            
+            await Testimonial.create({
+                nombre,
+                correo,
+                mensaje
+            });
+
+            res.redirect('/testimoniales');
+
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+}
+
+export {
+    guardarTestimonial
+}
